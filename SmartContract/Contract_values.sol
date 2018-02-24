a28af4f4c6b847f3ab5f84f470c93773eb1786e4
Erik Niemeijer <erik.niemeijer@ultimo.net>
2018-02-24 10:21:55 +0100

    Added ZION token contract

---
+++ SmartContract/Contract_values.sol
@@ -0,0 +1,6 @@
+// EKN: Add these values build contact (http://remix.ethereum.org/)
+//"ZION Token", "ZION", 18, "1.0", "0xAEf46875Eb00Ce14B5830b8de2e05aB79dC625d9", "0x1dEB6F7f7F2c4807cE287A8627681044547AB00A"
+
+// EKN: Add these values to build contact (http://remix.ethereum.org/)
+//"Your ETH Address", "ERC20 tokenAddress", "TokenCreationCap", "Rate", "StartTime in unixtimestamp"
+//"0x2b830933d565A2476eD1a424bdB4f7EE02dC37cc", "0x692a70d2e424a56d2c6c27aa97d1a86395877b3a", "100000000000000000000000000", "5000","1514764800"
